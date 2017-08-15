6a797e6213c2bb0f783a980b6c0ebdae42efb827
charminglee <charminglee911@qq.com>
2017-08-15 19:19:24 +0800

    二叉堆

---
+++ src/main/java/com/ming/test/BinaryHeap/BinaryHeap.java
@@ -0,0 +1,98 @@
+package com.ming.test.BinaryHeap;
+
+public class BinaryHeap<T extends Comparable<? super T>> {
+    private static final int DEFAULT_SIZE = 10;
+    private T[] array;
+    private int currentSize;
+
+    public BinaryHeap(){
+        array = (T[]) new Comparable[DEFAULT_SIZE];
+    }
+
+    public BinaryHeap(T[] items){
+        currentSize = items.length;
+        array = (T[])new Comparable[currentSize * 2 +1];
+        for (int i = 0; i < items.length; i++)
+            array[i+1] = items[i];
+
+        biuldHelp();
+    }
+
+    public void insert(T x){
+        if (x == null)
+            return;
+
+        percolateUp(x);
+    }
+
+    public T findMin(){
+        return array[1];
+    }
+
+    public T deleteMin(){
+        if (isEmpty())
+            return null;
+
+        T min = findMin();
+        array[1] = null;
+        percolateDown(1, array[currentSize--]);
+
+        return min;
+    }
+
+    public boolean isEmpty(){
+        return array == null ? true : (currentSize == 0);
+    }
+
+    public void print(){
+        if (array != null)
+            for (T x : array)
+                if (x != null)
+                    System.out.println(x);
+    }
+
+    private void biuldHelp(){
+        for (int i = currentSize/2; i > 0; i--) {
+            percolateDown(i, array[i]);
+        }
+    }
+
+    private void enlargeArray(int size){
+        T[] old = array;
+        array = (T[]) new Comparable[size];
+        for (int i = 0; i < old.length; i++)
+            array[i] = old[i];
+    }
+
+    private void percolateDown(int hole, T x){
+        int index;
+        while (hole * 2 <= currentSize){
+            index = hole*2;
+            if (array[index].compareTo(array[index+1]) > 0)
+                index++;
+
+            if (x.compareTo(array[index]) > 0)
+                array[hole] = array[index];
+            else
+                break;
+
+            hole = index;
+        }
+
+        array[hole] = x;
+    }
+
+    private void percolateUp(T x){
+        if (currentSize == array.length -1)
+            enlargeArray(array.length * 2 +1);
+
+        int hole = ++currentSize;
+        while (array[hole/2] != null && x.compareTo(array[hole/2]) < 0){
+            array[hole] = array[hole/2];
+            hole /= 2;
+        }
+
+        array[hole] = x;
+    }
+
+}
--- src/main/java/com/ming/test/Test.java
+++ src/main/java/com/ming/test/Test.java
@@ -1,15 +1,16 @@
 package com.ming.test;
 
+import com.ming.test.BinaryHeap.BinaryHeap;
+
 /**
  * Created by charminglee on 17-7-18.
  */
 public class Test {
-
     public static void main(String[] arg) {
+        Integer[] arr = {20, 1, 2, 3, 4, 5};
 
-        System.out.println("");
+        BinaryHeap<Integer> heap = new BinaryHeap<>(arr);
 
+        heap.print();
     }
-
-
 }
