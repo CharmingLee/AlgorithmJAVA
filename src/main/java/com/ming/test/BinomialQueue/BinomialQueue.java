0a1bce258a896921631a70a19752747a03a94832
charminglee <charminglee911@qq.com>
2017-08-24 17:56:22 +0800

    完成二项队列

--- src/main/java/com/ming/test/BinomialQueue/BinomialQueue.java
+++ src/main/java/com/ming/test/BinomialQueue/BinomialQueue.java
@@ -14,34 +14,144 @@ public class BinomialQueue <T extends Comparable<? super T>> {
         this.nodeTrees = new Node[DEFULL_SIZE];
     }
 
-    public BinomialQueue(Node<T> nodes){
-
+    public BinomialQueue(T x){
+        nodeTrees = new Node[DEFULL_SIZE];
+        nodeTrees[0] = new Node<T>(x);
+        currentSize++;
     }
 
     public void merge(BinomialQueue<T> rhs){
         if (this == rhs || rhs == null || rhs.currentSize < 1)
             return;
 
+        currentSize += rhs.currentSize;
+        if (currentSize > capacity()){
+            int maxSize = Math.max(rhs.nodeTrees.length, nodeTrees.length);
+            expandTheTrees(maxSize + 1);
+        }
 
+        int index = 0;
+        int j = 1;
+        Node<T> t1 = null;
+        Node<T> t2 = null;
+        Node<T> carry = null;
+        while (j <= currentSize){
+            t1 = nodeTrees[index];
+            t2 = index < rhs.nodeTrees.length ? rhs.nodeTrees[index] : null;
+
+            int whilCase = t1 == null ? 0 : 1;
+            whilCase += t2 == null ? 0 : 2;
+            whilCase += carry == null ? 0 : 4;
+
+            switch (whilCase){
+                case 0://all null
+                    break;
+                case 1://t1
+                    break;
+                case 2://t2
+                    nodeTrees[index] = t2;
+                    rhs.nodeTrees[index] = null;
+                    break;
+                case 3://t1 t2
+                    carry = comBineTrees(t1, t2);
+                    nodeTrees[index] = rhs.nodeTrees[index] = null;
+                    break;
+                case 4://carry
+                    nodeTrees[index] = carry;
+                    carry = null;
+                    break;
+                case 5://t1 carry
+                    carry = comBineTrees(t1, carry);
+                    nodeTrees[index] = null;
+                    break;
+                case 6://t2 carry
+                    carry = comBineTrees(t2, carry);
+                    rhs.nodeTrees[index] = null;
+                    break;
+                case 7://t1 t2 carry
+                    nodeTrees[index] = carry;
+                    carry = comBineTrees(t1, t2);
+                    rhs.nodeTrees[index] = null;
+                    break;
+            }
+
+            index++;
+            j *= 2;
+        }
+
+        rhs.currentSize = 0;
+        rhs.nodeTrees = null;
     }
 
-    public void insert(Node<T> x){
-
+    public void insert(T x){
+        if (x != null)
+            merge(new BinomialQueue<T>(x));
     }
 
     public T findMin(){
+        int index = findMinNode();
+        if (index > -1)
+            return nodeTrees[index].data;
+
         return null;
     }
 
     public T delMin(){
-        return null;
+        int minIndex = findMinNode();
+        Node<T> delNode = nodeTrees[minIndex].childrenNode;
+        T minData = nodeTrees[minIndex].data;
+
+        BinomialQueue<T> delQueue = new BinomialQueue<>();
+        delQueue.expandTheTrees(minIndex);
+        delQueue.currentSize = (1 << minIndex)-1;
+
+        for (int i = minIndex -1; i > -1; i--) {
+            delQueue.nodeTrees[i] = delNode;
+            delNode = delNode.nextNode;
+            delQueue.nodeTrees[i].nextNode = null;
+        }
+
+        nodeTrees[minIndex] = null;
+        currentSize -= delQueue.currentSize + 1;
+        merge(delQueue);
+
+        return minData;
+    }
+
+    private int capacity(){
+        return (1 << nodeTrees.length) -1;
+    }
+
+    private void expandTheTrees(int size){
+        Node<T>[] newTree = new Node[size];
+        for (int i = 0; i < Math.min(size, nodeTrees.length); i++)
+            newTree[i] = nodeTrees[i];
+
+        nodeTrees = newTree;
+    }
+
+    private int findMinNode(){
+        int min = -1;
+
+        for (int i = 0; i < nodeTrees.length; i++){
+            if (min == -1 && nodeTrees[i] != null){
+                min = i;
+                break;
+            }
+
+            if (nodeTrees[i] != null && min > -1)
+                if (nodeTrees[min].data.compareTo(nodeTrees[i].data) > 0)
+                    min = i;
+        }
+
+        return min;
     }
 
     private Node<T> comBineTrees(Node<T> t1, Node<T> t2){
         if (t1.data.compareTo(t2.data) > 0)
             return comBineTrees(t2, t1);
-        t2.nextNode = t1.childeNode;
-        t1.childeNode = t2;
+        t2.nextNode = t1.childrenNode;
+        t1.childrenNode = t2;
         return t1;
     }
 
@@ -51,16 +161,16 @@ public class BinomialQueue <T extends Comparable<? super T>> {
 
     private static class Node<T>{
         public T data;
-        public Node<T> childeNode;
+        public Node<T> childrenNode;
         public Node<T> nextNode;
 
         public Node(T data) {
             this(data, null, null);
         }
 
-        public Node(T data, Node<T> childeNode, Node<T> nextNode) {
+        public Node(T data, Node<T> childrenNode, Node<T> nextNode) {
             this.data = data;
-            this.childeNode = childeNode;
+            this.childrenNode = childrenNode;
             this.nextNode = nextNode;
         }
     }
--- src/main/java/com/ming/test/Test.java
+++ src/main/java/com/ming/test/Test.java
@@ -1,10 +1,21 @@
 package com.ming.test;
 
+import com.ming.test.BinomialQueue.BinomialQueue;
+
 /**
  * Created by charminglee on 17-7-18.
  */
 public class Test {
     public static void main(String[] arg) {
+        BinomialQueue<Integer> newQueue = new BinomialQueue<>();
+        for (int i = 36; i > 0; i--) {
+            newQueue.insert(i);
+        }
 
+
+        int len = newQueue.getCurrentSize();
+        for (int i = 0; i < len; i++) {
+            System.out.println(newQueue.delMin());
+        }
     }
 }
