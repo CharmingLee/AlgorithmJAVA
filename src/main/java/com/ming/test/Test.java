e3a0d18d478e4166b89dcea6f9d5ce50e49f5819
charminglee <charminglee911@qq.com>
2017-08-25 17:56:23 +0800

    创建排序算法类

--- src/main/java/com/ming/test/Test.java
+++ src/main/java/com/ming/test/Test.java
@@ -1,21 +1,24 @@
 package com.ming.test;
 
-import com.ming.test.BinomialQueue.BinomialQueue;
+import com.ming.test.Sort.Sort;
 
 /**
  * Created by charminglee on 17-7-18.
  */
 public class Test {
     public static void main(String[] arg) {
-        BinomialQueue<Integer> newQueue = new BinomialQueue<>();
-        for (int i = 36; i > 0; i--) {
-            newQueue.insert(i);
+        Integer[] arr = new Integer[10];
+        for (int i = 0,j = 10; j > 0 ; j--,i++) {
+            arr[i] = j;
         }
 
 
-        int len = newQueue.getCurrentSize();
-        for (int i = 0; i < len; i++) {
-            System.out.println(newQueue.delMin());
+        Sort<Integer> s = new Sort<>();
+        s.shellSort(arr);
+
+        for (Integer i : arr) {
+            System.out.println(i);
         }
+
     }
 }
