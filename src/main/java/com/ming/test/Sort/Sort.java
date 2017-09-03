0020545451f1647c9ce218b6ace02a68839e9ea5
charminglee <charming_lee@qq.com>
2017-09-03 10:23:24 +0800

    1.完不同长度成字符串排序 2.Merge branch 'master' of https://git.oschina.net/CharmingLee/TestJava
    # Conflicts:
    #	src/main/java/com/ming/test/Test.java

--- src/main/java/com/ming/test/Sort/Sort.java
+++ src/main/java/com/ming/test/Sort/Sort.java
@@ -1,5 +1,7 @@
 package com.ming.test.Sort;
 
+import java.util.ArrayList;
+
 public class Sort<T extends Comparable<? super T>> {
 
     /**
@@ -35,6 +37,91 @@ public class Sort<T extends Comparable<? super T>> {
             }
         }
 
+
+    }
+
+    /**
+     * 单一长度字符串(ascii 256以下)排序
+     * @param arr
+     * @param stringLen
+     */
+    public static void singleLenStringSort(String[] arr, int stringLen){
+        final int BUCKETS = 256;
+        ArrayList<String>[] buckets = new ArrayList[BUCKETS];
+
+        for (int i = 0; i < BUCKETS; i++) {
+            buckets[i] = new ArrayList<>();
+        }
+
+        for (int pos = stringLen - 1; pos >= 0; pos--) {
+            for (String s : arr) {
+                buckets[s.charAt(pos)].add(s);
+            }
+
+            int idx = 0;
+            for (ArrayList<String> thisBuckets : buckets) {
+                for (String s : thisBuckets) {
+                    arr[idx++] = s;
+                }
+
+                thisBuckets.clear();
+            }
+        }
+
+    }
+
+    /**
+     * 不同长度字符串(ascii 256以下)数组排序
+     * @param arr
+     * @param maxLen
+     */
+    public static void stringSort(String[] arr, int maxLen){
+        //为每个长度的字符串创建一个桶
+        ArrayList<String>[] stringByLen = new ArrayList[maxLen + 1];
+        for (int i = 0; i < stringByLen.length; i++) {
+            stringByLen[i] = new ArrayList<String>();
+        }
+
+        //不同ascii码的字符串创建一个桶
+        final int BUCKETS = 256;
+        ArrayList<String>[] buckets = new ArrayList[BUCKETS];
+        for (int i = 0; i < buckets.length; i++) {
+            buckets[i] = new ArrayList<String>();
+        }
+
+        //把字符串按照长度放到对应的桶里，stringByLen数组的下标表示字符串的长度
+        for (int i = 0; i < arr.length; i++) {
+            stringByLen[arr[i].length()].add(arr[i]);
+        }
+
+        //按照数组下标遍历每个桶并放回到arr数组中，这时数组中的字符串已经按照长度不同排序
+        int index = 0;
+        for (ArrayList<String> strLenArr : stringByLen) {
+            for (String s : strLenArr) {
+                arr[index++] = s;
+            }
+        }
+
+        int startIndex = arr.length;
+        for (int pos = maxLen - 1; pos >= 0 ; pos--) {
+            //startIndex表示pos + 1长度的字符串在arr中下标的起点
+            startIndex -= stringByLen[pos + 1].size();
+            //按当前字符在ascii中对应的数值放入对应下标的桶里
+            for (int i = startIndex; i < arr.length; i++) {
+                buckets[arr[i].charAt(pos)].add(arr[i]);
+            }
+
+            //从桶中按下标从小到大去除字符串并放到数组相应位置
+            int idx = startIndex;
+            for (ArrayList<String> strArr : buckets) {
+                for (String s : strArr) {
+                    arr[idx++] = s;
+                }
+
+                strArr.clear();
+            }
+
+        }
 
     }
 
--- src/main/java/com/ming/test/Test.java
+++ src/main/java/com/ming/test/Test.java
@@ -1,69 +1,19 @@
 package com.ming.test;
 
-import java.util.ArrayList;
+import com.ming.test.Sort.Sort;
+
 import java.util.Arrays;
 
 /**
  * Created by charminglee on 17-7-18.
  */
 public class Test {
+
     public static void main(String[] arg) {
-        String[] arr = {"abs","vbd","bbf","Abg","Cbr","Bbw"};
-
-        sort(arr, 3);
-        System.out.println(Arrays.toString(arr));
-
-        sort1(arr, 3);
-        System.out.println(Arrays.toString(arr));
-
+        String[] str = {"cdv", "ecvd", "bds", "atv", "dffgg"};
+        Sort.stringSort(str, 5);
+        System.out.println(Arrays.toString(str));
     }
 
-    private static void sort1(String[] arr, int stringLen){
-        final int BUCKETS = 256;
-        ArrayList<String>[] buckets = new ArrayList[BUCKETS];
-
-        for (int i = 0; i < BUCKETS; i++) {
-            buckets[i] = new ArrayList<>();
-        }
-
-        for (String s : arr) {
-            buckets[s.charAt(0)].add(s);
-        }
-
-        int idx = 0;
-        for (ArrayList<String> thisBuckets : buckets) {
-            for (String s : thisBuckets) {
-                arr[idx++] = s;
-            }
-
-            thisBuckets.clear();
-        }
-
-    }
-
-    private static void sort(String[] arr, int stringLen){
-        final int BUCKETS = 256;
-        ArrayList<String>[] buckets = new ArrayList[BUCKETS];
-
-        for (int i = 0; i < BUCKETS; i++) {
-            buckets[i] = new ArrayList<>();
-        }
-
-        for (int pos = stringLen - 1; pos >= 0; pos--) {
-            for (String s : arr) {
-                buckets[s.charAt(pos)].add(s);
-            }
-
-            int idx = 0;
-            for (ArrayList<String> thisBuckets : buckets) {
-                for (String s : thisBuckets) {
-                    arr[idx++] = s;
-                }
-
-                thisBuckets.clear();
-            }
-        }
-
-    }
 
 }
