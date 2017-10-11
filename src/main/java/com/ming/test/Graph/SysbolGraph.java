8334e6b3afef82a13c152582e8c1685dd0c2aa7e
charminglee <charminglee911@qq.com>
2017-10-11 16:37:37 +0800

    增加符号图

---
+++ src/main/java/com/ming/test/Graph/SysbolGraph.java
@@ -0,0 +1,93 @@
+package com.ming.test.Graph;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 符号图
+ * Created by charminglee on 17-10-11.
+ */
+public class SysbolGraph {
+    private Map<String, Integer> st;
+    private String[] keys;
+    private Graph G;
+
+    /**
+     *
+     * JFK MCO
+     * ORD DEM
+     * ORD HOU
+     * DFW PHK
+     * JFK ATL
+     * ORD DFW
+     * ORD PHX
+     * ATL HOU
+     * DEN PHX
+     * PHX LAX
+     * JFK ORD
+     * DEN LAS
+     * DFW HOU
+     * ORD ATL
+     * LAS LAX
+     * ATL MCO
+     * HOU MCO
+     * LAS PHX
+     * @param delim
+     * @throws IOException
+     */
+
+    public SysbolGraph(String fileName, String delim) throws IOException {
+        st = new HashMap<>();
+        File file = new File(fileName);
+        FileInputStream fin = new FileInputStream(file);
+        InputStreamReader reader = new InputStreamReader(fin);
+        BufferedReader buff = new BufferedReader(reader);
+        String line = null;
+        buff.mark((int) (file.length() + 1));
+        while ( (line = buff.readLine()) != null){
+            String[] split = line.split(delim);
+
+            for (String str : split)
+                if (!st.containsKey(str))
+                    st.put(str, st.size());
+        }
+
+        keys = new String[st.size()];
+        for (String key : st.keySet())
+            keys[st.get(key)] = key;
+
+        G = new Graph(st.size());
+        buff.reset();
+
+        while ( (line = buff.readLine()) != null){
+            String[] split = line.split(delim);
+            G.addEde(st.get(split[0]), st.get(split[1]));
+        }
+
+    }
+
+    public boolean contains(String key){
+        return st.containsKey(key);
+    }
+
+    public Integer index(String key){
+        return st.get(key);
+    }
+
+    public String name(int index){
+        if (index > keys.length - 1)
+            return null;
+
+        return keys[index];
+    }
+
+    public Graph G(){
+        return G;
+    }
+
+}
--- src/main/java/com/ming/test/Test.java
+++ src/main/java/com/ming/test/Test.java
@@ -1,7 +1,9 @@
 package com.ming.test;
 
 import com.ming.test.Graph.BreadthFirstPath;
+import com.ming.test.Graph.DepthFirstPath;
 import com.ming.test.Graph.Graph;
+import com.ming.test.Graph.SysbolGraph;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -12,11 +14,16 @@ import java.io.IOException;
 public class Test {
 
     public static void main(String[] arg) throws IOException {
-        FileInputStream fin = new FileInputStream("/home/charminglee/桌面/a.txt");
-        Graph g = new Graph(fin);
+//        FileInputStream fin = new FileInputStream("/home/charminglee/桌面/routes.txt");
+        SysbolGraph sg = new SysbolGraph("/home/charminglee/桌面/routes.txt", " ");
+        Graph g = sg.G();
+        BreadthFirstPath path = new BreadthFirstPath(g, sg.index("LAS"));
 
-        BreadthFirstPath path = new BreadthFirstPath(g, 0);
-        System.out.println(path.PathTo(6));
+        Iterable<Integer> integers = path.PathTo(sg.index("MCO"));
+
+        for (Integer i : integers) {
+            System.out.println(sg.name(i));
+        }
     }
 
 
